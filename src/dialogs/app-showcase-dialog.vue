432d0d64aae466a6b4723a4509c3decaf8f26c7f
Rost Talnax <rosttalnax@Rosts-MacBook-Pro.local>
2020-04-30 15:55:12 -0700

    36 check-in...

--- src/dialogs/app-showcase-dialog.vue
+++ src/dialogs/app-showcase-dialog.vue
@@ -2,67 +2,33 @@
     <div class="communication-more-dialog">
         <div class="text-center">
 
-            <v-dialog v-model="show_dlg" width="690">
+            <v-dialog v-model="show_dlg" width="810">
 
                 <v-card>
 
                     <v-card-title class="headline teal white--text title-card">
                         <v-icon style="margin-right: 15px;">event_note</v-icon>
-
-                        {{this.baseValue.heading}}<!---->
-                        <!--{{this.baseValue.img}}-->
+                        {{this.baseValue.heading}}
                     </v-card-title>
 
-                    <v-card-text><br>
-                        <!--
-                            <v-img
-                                class="white--text align-end"
-                                height="240px"
-                                :src="getImgUrl(this.baseValue.img)">
-
-                            </v-img>
-                            -->
-
+                    <v-card-text>
+                        <br>
+                        <div class="showcase-subtitle">
+                            <b>{{this.baseValue.when}}</b>
+                        </div>
+                        <br>
+                        <div class="showcase-explanation">
+                            {{this.baseValue.explanation_1}} <br>
+                            {{this.baseValue.explanation_2}} <br>
+                        </div>
+                        <br>
                         <v-img
-                                class="white--text align-end"
-                                height="240px"
-                                :src="getImgUrl('webinar/rsi-aris.png')">
-
+                            class="white--text align-end"
+                            height="420px"
+                            :src="getImgUrl('bk/rsi-aris.png')">
                         </v-img>
 
-                        <!--'bk/rsi-logo.jpg'-->
 
-
-
-                        <!--
-                        Scheduled System Maintenance 4/14/2020,  9:50:17 AM <br><br>
-
-                        <b>Tuesday, April 14, 2020 9:29:40 AM EST</b>
-                        Please note that <b>RSi</b> will be conducting routine system maintenance on Thursday, April 23,
-                        2020 at 4:00PM EST for 4 hours. <br>
-                        <b>RSi</b> application will be available, but FTP will be inaccessible
-                        and reports will not be delivered to FTP site while maintenance is being performed for the below:<br><br>
-
-                        If you have any concerns or require access to RSi;s solutions during this scheduled time,
-                        please notify immediately so that we can reschedule accordingly. <br><br>
-
-                        <b>Start time:</b> <br>
-                        Providence: 23/Apr/2020 04:00 PM EDT  <br>
-                        Bentonville: 23/Apr/2020 03:00 PM CDT <br>
-                        Mountain View: 23/Apr/2020 01:00 PM PDT <br>
-                        Shanghai: 24/Apr/2020 04:00 AM CST <br>
-                        Pune: 24/Apr/2020 01:30 AM IST <br>
-                        London: 23/Apr/2020 09:00 PM BST <br><br>
-
-                        <b>Duration:</b> 4 hours
-
-                        <br><br>
-                        If you have any questions or concerns about the maintenance performed, <br>
-                        please submit request or send us a Chat via the <b>Knowledge Hub</b> or email us<br>
-                        at <b>support@retailsolutions.com</b> <br><br>
-
-                        We appreciate your patience during tis time.
-                        -->
                     </v-card-text>
 
                     <v-divider></v-divider>
@@ -123,6 +89,17 @@
 
     .v-application .headline {
         font-size: 1.2rem !important;
+    }
+
+    .showcase-subtitle {
+        font-size: 18px !important;
+        margin: auto;
+        text-align: center;
+    }
+
+    .showcase-explanation {
+        font-size: 18px;
+        text-align: center;
     }
 
 </style>
